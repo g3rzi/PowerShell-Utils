7dc81199fe35a0af926b1a083da72515a642faa3
Eviatar Gerzi <evyatar575@gmail.com>
2017-12-27 15:30:27 +0200

    Add files via upload

---
+++ Convert-FileToArrayBytesFile.ps1
@@ -0,0 +1,48 @@
+﻿function Convert-FileToArrayBytesFile
+{
+<#
+	.SYNOPSIS
+		Author: Eviatar Gerzi (@g3rzi)
+			
+	.DESCRIPTION
+
+
+	.PARAMETER FileToConvertPath
+		The file you want to convert
+
+	.PARAMETER NewFilePath
+		The target file that will contain the text
+			
+	.EXAMPLE
+        Convert-FileToArrayBytesFile -FileToConvertPath "C:\tmp\1.txt" -NewFilePath "c:\tmp\calc3.txt"
+
+#>
+
+    [CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[string]$FileToConvertPath,
+		[Parameter(Mandatory = $true)]
+		[string]$NewFilePath
+	)
+	
+    
+    $bytes = [System.IO.File]::ReadAllBytes($FileToConvertPath)
+    
+    $sb = New-Object -TypeName "System.Text.StringBuilder";
+
+    $prefix = ""
+    $sb.Append("@(") | Out-Null
+    $bytes | % {
+        
+        $sb.Append($prefix)
+        $prefix = ","
+        $sb.Append($_) | Out-Null
+    }
+
+    $sb.Append(")") | Out-Null
+
+    $sb.ToString() >> $NewFilePath
+}
+
