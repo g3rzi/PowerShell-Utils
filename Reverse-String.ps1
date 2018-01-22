0ea30e5d10e801d49997b27794431669517a81c9
Eviatar Gerzi <evyatar575@gmail.com>
2018-01-22 11:18:06 +0200

    Add files via upload

---
+++ Reverse-String.ps1
@@ -0,0 +1,28 @@
+function Reverse-String
+{
+<#
+	.SYNOPSIS
+		Author: Eviatar Gerzi (@g3rzi)
+			
+	.DESCRIPTION
+		Reverse string.
+		Based on articale from Microsoft:
+		https://blogs.technet.microsoft.com/heyscriptingguy/2015/11/04/reverse-strings-with-powershell/
+		
+	.PARAMETER StringToReverse
+		The string to reverse
+
+	.EXAMPLE
+        	Reverse-String "abc"
+			# Output: "cba"
+#>
+
+  Param(
+    [string]$StringToReverse
+  )
+
+  $charArray = $StringToReverse.ToCharArray()
+  [array]::Reverse($charArray) | Out-Null
+  $reversedString = -join($charArray)
+  return $reversedString
+}
